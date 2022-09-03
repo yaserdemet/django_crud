8660d2ee0e686781614b67eb21d015f1a7a237d3
yaser <ddemety372@gmail.com>
2022-09-03 13:07:07 +0300

    log

--- main/settings.py
+++ main/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import django_heroku
+
 from pathlib import Path
 from decouple import config
+import django_heroku
 
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +128,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+#? Activate Django-Heroku 👇:
+django_heroku.settings(locals())
